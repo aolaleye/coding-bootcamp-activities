18503f74f9d1327933476aba9a37a2341d7ff2ac
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-03-25 22:18:17 -0400

    updated practice files from March 21

--- class-activities/mar-21/16-Callbacks/Unsolved/callbacks.js
+++ class-activities/mar-21/16-Callbacks/Unsolved/callbacks.js
@@ -2,9 +2,17 @@ var fs = require("fs");
 
 // Write a function that logs a message, then executes
 // a function argument.
+function writeMessage(message) {
+    console.log(message);
+}
 
 // Write a function that runs a function argument if
 // its other argument is truthy.
+function truthy(yes, go) {
+    if(yes === true) {
+        go;
+    }
+}
 
 // Write a function that accepts a function argument and
 // a value, and returns a function that returns the result
