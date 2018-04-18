4683fa6d7a5df85be16a2465120d5bf945b6c4a7
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-18 11:35:52 -0400

    updated practice files from April 13

--- class-activities/apr-13/20-Scraping-With-Mongoose/Unsolved/server.js
+++ class-activities/apr-13/20-Scraping-With-Mongoose/Unsolved/server.js
@@ -71,6 +71,16 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // TODO: Finish the route so it grabs all of the articles
+  
+  db.Article.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
@@ -80,6 +90,18 @@ app.get("/articles/:id", function(req, res) {
   // Finish the route so it finds one article using the req.params.id,
   // and run the populate method with "note",
   // then responds with the article with the note included
+  db.Article.findOne({
+    _id: req.params.id
+  })
+  .populate ("note")
+  .then(function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
 });
 
 // Route for saving/updating an Article's associated Note
@@ -89,6 +111,17 @@ app.post("/articles/:id", function(req, res) {
   // save the new note that gets posted to the Notes collection
   // then find an article from the req.params.id
   // and update it's "note" property with the _id of the new note
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({}, { $push: { _id: req.params.id, note: dbNote._id } }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+
 });
 
 // Start the server
