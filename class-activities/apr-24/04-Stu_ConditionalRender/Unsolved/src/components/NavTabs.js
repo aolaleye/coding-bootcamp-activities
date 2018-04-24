023af373088ed77e532195c69ddf4ef90aa5f0bb
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-24 14:11:21 -0400

    updated practice files from April 24

--- class-activities/apr-24/04-Stu_ConditionalRender/Unsolved/src/components/NavTabs.js
+++ class-activities/apr-24/04-Stu_ConditionalRender/Unsolved/src/components/NavTabs.js
@@ -3,22 +3,22 @@ import React from "react";
 const NavTabs = props => (
   <ul className="nav nav-tabs">
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("Home")} className="nav-link">
+      <a onClick={() => props.handlePageChange("Home")} className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}>
         Home
       </a>
     </li>
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("About")} className="nav-link">
+      <a onClick={() => props.handlePageChange("About")} className={props.currentPage === "About" ? "nav-link active" : "nav-link"}>
         About
       </a>
     </li>
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("Blog")} className="nav-link">
+      <a onClick={() => props.handlePageChange("Blog")} className={props.currentPage === "Blog" ? "nav-link active" : "nav-link"}>
         Blog
       </a>
     </li>
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("Contact")} className="nav-link">
+      <a onClick={() => props.handlePageChange("Contact")} className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}>
         Contact
       </a>
     </li>
--- class-activities/apr-24/116-ReactDemo/react-practice/src/components/NavTabs.js
+++ class-activities/apr-24/116-ReactDemo/react-practice/src/components/NavTabs.js
@@ -1,26 +1,57 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const NavTabs = props => (
+const NavTabs = () => (
   <ul className="nav nav-tabs">
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("Home")} className="nav-link">
+      <Link
+        to="/"
+        className={
+          window.location.pathname === "/" ? "nav-link active" : "nav-link"
+        }
+      >
         Home
-      </a>
+      </Link>
     </li>
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("About")} className="nav-link">
+      <Link
+        to="/about"
+        className={
+          window.location.pathname === "/about" ? "nav-link active" : "nav-link"
+        }
+      >
         About
-      </a>
+      </Link>
     </li>
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("Blog")} className="nav-link">
+      <Link
+        to="/blog"
+        className={
+          window.location.pathname === "/blog" ? "nav-link active" : "nav-link"
+        }
+      >
         Blog
-      </a>
+      </Link>
     </li>
     <li className="nav-item">
-      <a onClick={() => props.handlePageChange("Contact")} className="nav-link">
+      <Link
+        to="/contact"
+        className={
+          window.location.pathname === "/contact" ? "nav-link active" : "nav-link"
+        }
+      >
         Contact
-      </a>
+      </Link>
+    </li>
+    <li className="nav-item">
+      <Link
+        to="/contact/learn"
+        className={
+          window.location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"
+        }
+      >
+        Learn
+      </Link>
     </li>
   </ul>
 );
